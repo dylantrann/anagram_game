c35d0f1284c699c179f9b56c60b1f8b95677c922
BennyXie <118026674+BennyXie@users.noreply.github.com>
2024-04-06 17:45:04 -0700

    bug-free code, but need to be optimized

--- AnagramGenerator.java
+++ AnagramGenerator.java
@@ -1,9 +1,12 @@
+package anagram_game;
+
 import java.util.*;
 import java.net.*;
 import java.io.*;
 
 public class AnagramGenerator {
 
+    private static int counter = 0;
     private static boolean found = false;
     private static List<String> results;
 
@@ -28,6 +31,7 @@ public class AnagramGenerator {
         if (results == null)
             System.out.println("No anagram available");
         else {
+            System.out.println("Found it! Anagrams are:");
             for (String result : results)
                 System.out.println(result);
         }
@@ -35,12 +39,15 @@ public class AnagramGenerator {
 
     private static void generateAnagrams(char[] chars, int index) throws IOException {
         // If we have reached the end of the array, we have generated an anagram
-        System.out.println("gg");
         if (found)
             return;
+
+        counter++;
+        System.out.println(counter);
         if (index == chars.length) {
 //            anagrams.add(new String(chars));
-            List<String> words = separate(chars.toString());
+            System.out.println(new String(chars));
+            List<String> words = separate(new String(chars));
             results = getAnagramFromWeb(words);
             if (results != null)
                 found = true;
@@ -52,6 +59,8 @@ public class AnagramGenerator {
             swap(chars, index, i);
             generateAnagrams(chars, index + 1);
             swap(chars, index, i);
+            if (found)
+                return;
         }
     }
 
@@ -61,14 +70,19 @@ public class AnagramGenerator {
         {
             String first = chars.substring(0, i);
             String second = chars.substring(i);
-            if (first.length() <= 10) list.add(first);
-            if (second.length() <= 10) list.add(second);
+            if (first.length() <= 10 && second.length() <= 10) {
+                list.add(first);
+                list.add(second);
+                return list;
+            }
+
         }
         return list;
     }
 
     private static List<String> getAnagramFromWeb(List<String> words) throws IOException {
         List<String> list = new ArrayList<>();
+        System.out.println("Content of words: " + words);
         for (String word : words)
         {
             // Get apiResult from web;
@@ -89,10 +103,16 @@ public class AnagramGenerator {
             }
             in.close();
 
+            for (String w : best)
+                System.out.println(w);
             String apiResult = best.get(0);
-            if (apiResult.length() != word.length())
+//            System.out.println(apiResult);
+            if (apiResult.length() != word.length()) {
+                list.clear();
                 return null;
+            }
             list.add(apiResult);
+            System.out.println("List content: " + list);
         }
         return list;
     }
